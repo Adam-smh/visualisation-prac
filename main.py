41a320409a86fcf6ecf785ee72ead74280952be2
Adam-smh <africa.adam1@gmail.com>
2021-05-31 16:35:15 +0200

    Add

---
+++ main.py
@@ -0,0 +1,40 @@
+from numpy import *
+from scipy import stats
+import matplotlib.pyplot as plt
+
+
+# test_names =["Andy", "Martin", "Zahara", "Vuyo","Ziyaad"]
+# test_scores = [12,99,65,85,42]
+# x_pos = [i for i, _ in enumerate(test_names)]
+#
+# plt.bar(x_pos, test_scores, color='cyan')
+# plt.xlabel("names")
+# plt.ylabel("score")
+# plt.title("test scores and names")
+# plt.xticks(x_pos, test_names)
+# plt.show()
+#
+# x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
+# Y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+#
+# import matplotlib.pyplot as plt
+#
+# x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
+# y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+#
+# plt.scatter(x, y)
+# plt.show()
+#
+
+# a=[1.3, 3.4, 2.3, 9.8]
+# b=[3.5, 4.9, 1.3, 2.2]
+# c=[9.7, 1.5, 4.3, 0.9, 11.2]
+# data1=[a,b,c]
+# plt.subplot(121)
+# plt.boxplot(data1)
+# plt.xlabel("Colleges")
+# plt.ylabel("Scores")
+# plt.title("Boxplot for colleges")
+# plt.subplot(122)
+# plt.hist(data1, bins=5)
+# plt.show()
